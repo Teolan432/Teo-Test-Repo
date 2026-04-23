e6fa041df57eca565b2ce1fa0eeb54720a3f6d6e
Teolan432 <teolan.govender@sambeconsulting.com>
2026-04-23 15:29:18 +0200

    added first custom js action - 5

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const github = reqiure('@actions/github');
+const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
